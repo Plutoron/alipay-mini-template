853f1a6c30866c3f23c2837165747d6146c821b6
yunlong.syl <yunlong.syl@dtwave-inc.com>
2019-03-08 22:02:19 +0800

    io-context 添加restful

--- common/io-context.js
+++ common/io-context.js
@@ -11,10 +11,27 @@ const ioContext = async ({
   dataType = 'json',
   data = {}
 }) => {
+  let isRestful 
+
+	const dataKeys = Object.keys(data)
+	const restParam = []
+
+	if (dataKeys.length > 0) {
+		if (dataKeys.some(v => v.indexOf(':') > -1)) {
+			isRestful = true
+			dataKeys.map((v, i) => {
+				if (v.indexOf(':') > -1) {
+					restParam.push(data[v])
+					delete data[v]
+				}
+			})
+		}
+  }
+  
   // 所有的请求，header默认携带token
   const res = await new Promise((resolve, reject) => {
     my.httpRequest({
-      url: `${https ? 'https' : 'http'}://${prefix[mode]}/${url}`,
+      url: `${https ? 'https' : 'http'}://${prefix[mode]}/${url}${isRestful ? '/' + restParam.join('/') : ''}`,
       headers: {
         'Content-Type': 'application/json',
         token: my.getStorageSync('token') || '',
